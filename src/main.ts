25aa899b91330bde3378a219a29965931b38f181
randazzo <angelo.randazzo@gmail.com>
2022-06-03 20:10:10 +0200

    re-baseline

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import './assets/app.css';
+
 import App from './App.vue';
-import CustomShape from "./components/CustomShape.vue";
-import Ellipse from "./components/Ellipse.vue";
+import CustomShape from "./components/diagram-editor/CustomShape.vue";
+import Ellipse from "./components/diagram-editor/Ellipse.vue";
 import { createApp } from 'vue';
 
 const app = createApp(App);
 
 app.component('CustomShape', CustomShape);
-app.component('Ellipse', Ellipse);
+app.component('Ellipse',     Ellipse);
 
 app.mount('#app')
