63446e8dbb71a1f1380ddfe727df750d7ffe9d2f
randazzo <angelo.randazzo@gmail.com>
2022-07-18 17:49:19 +0200

    added different inspector models

--- src/components/diagram-editor/item-properties.ts
+++ src/components/diagram-editor/item-properties.ts
@@ -1,45 +1,28 @@
 import { InspectorTab, PropertyType } from '../inspector/types';
 import { ObjectInspectorModel, ObjectProperty } from './../inspector/types';
+import { ConnectionStyle, ConnectionType } from './types';
 
-const id$    : ObjectProperty = { name: "id",     label: "ID",   type: PropertyType.TEXT,  editorFullsize: true, readonly: true, formatValue: (p, obj) => obj[p.name] + ' (' + obj.component + ')' };
-const title$ : ObjectProperty = { name: "title",  label: "Text", type: PropertyType.TEXT,  editorFullsize: true };
+export const id$    : ObjectProperty = { name: "id",     label: "ID",   type: PropertyType.TEXT,  editorFullsize: true, readonly: true, formatValue: (p, obj) => obj[p.name] + ' (' + obj.component + ')' };
+export const title$ : ObjectProperty = { name: "title",  label: "Text", type: PropertyType.TEXT,  editorFullsize: true };
 
-const fontSize$ : ObjectProperty = { name: "fontSize", label: "Font Size",  type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 4, max: 120, step: 1 } };
+export const fontSize$ : ObjectProperty = { name: "fontSize", label: "Font Size",  type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 4, max: 120, step: 1 } };
 
-const backColor$ : ObjectProperty = { name: "backgroundColor",  label: "Back Color", type: PropertyType.COLOR };
-const textColor$ : ObjectProperty = { name: "textColor",        label: "Text Color", type: PropertyType.COLOR };
+export const backColor$ : ObjectProperty = { name: "backgroundColor",  label: "Back Color", type: PropertyType.COLOR };
+export const textColor$ : ObjectProperty = { name: "textColor",        label: "Text Color", type: PropertyType.COLOR };
 
 
-const x$ : ObjectProperty = { name: "x", label: "X",      type: PropertyType.NUMBER };
-const y$ : ObjectProperty = { name: "y", label: "Y",      type: PropertyType.NUMBER };
-const w$ : ObjectProperty = { name: "w", label: "Width",  type: PropertyType.NUMBER };
-const h$ : ObjectProperty = { name: "h", label: "Height", type: PropertyType.NUMBER };
+export const x$ : ObjectProperty = { name: "x", label: "X",      type: PropertyType.NUMBER };
+export const y$ : ObjectProperty = { name: "y", label: "Y",      type: PropertyType.NUMBER };
+export const w$ : ObjectProperty = { name: "w", label: "Width",  type: PropertyType.NUMBER };
+export const h$ : ObjectProperty = { name: "h", label: "Height", type: PropertyType.NUMBER };
 
-const rotate$  : ObjectProperty = { name: "r",             label: "Rotation",  type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: -360, max: 360, step: 1 }};
-const radius$  : ObjectProperty = { name: "borderRadius",  label: "Radius",    type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 0,    max: 100, step: 1 }};
-const opacity$ : ObjectProperty = { name: "opacity",       label: "Opacity %", type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 0,    max: 100, step: 1 }};
-
-const styleTab : InspectorTab =  {
-    title: "Style",
-    sections: [        
-        {   // Style
-            name: "style",
-            title: "Text and style",
-            properties: [ id$, title$, fontSize$, backColor$, textColor$, opacity$ ]
-        },         
-        {   // Position & size
-            name: "pos_size",
-            title: "Position and size",
-            properties: [x$, y$, w$, h$, rotate$, radius$] 
-        }, 
-        
-    ] // sections
-}; // tab
+export const rotate$  : ObjectProperty = { name: "r",             label: "Rotation",  type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: -360, max: 360, step: 1 }};
+export const radius$  : ObjectProperty = { name: "borderRadius",  label: "Radius",    type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 0,    max: 100, step: 1 }};
+export const opacity$ : ObjectProperty = { name: "opacity",       label: "Opacity %", type: PropertyType.RANGE, editorFullsize: true, editorOptions: { min: 0,    max: 100, step: 1 }};
 
 const otherTab : InspectorTab =
 {
     title: "Other",
-
     sections: [
         {
             name: "comp_info",
@@ -51,8 +34,95 @@ const otherTab : InspectorTab =
     ] // sections
 }; // tab
 
-const model: ObjectInspectorModel = {
-    tabs: [ styleTab, otherTab ]
+
+export const basicModel: ObjectInspectorModel = {
+    tabs: [ 
+        {
+            title: "Style",
+            sections: [        
+                {   // Style
+                    name: "style",
+                    title: "Style",
+                    properties: [id$, backColor$, opacity$]
+                },         
+                {   // Position & size
+                    name: "pos_size",
+                    title: "Position and size",
+                    properties: [x$, y$, w$, h$, rotate$] 
+                }, 
+                
+            ] // sections
+        },
+        otherTab
+    ]
 }
 
-export default model;
+
+
+export const shapeModel: ObjectInspectorModel = {
+    tabs: [ 
+        {
+            title: "Style",
+            sections: [        
+                {   // Style
+                    name: "style",
+                    title: "Text and style",
+                    properties: [ id$, title$, fontSize$, backColor$, textColor$, opacity$ ]
+                },         
+                {   // Position & size
+                    name: "pos_size",
+                    title: "Position and size",
+                    properties: [x$, y$, w$, h$, rotate$, radius$] 
+                }, 
+                
+            ] // sections
+        },
+        otherTab
+    ]
+}
+
+export const shapeWithTextModel: ObjectInspectorModel = {
+    tabs: [ 
+        {
+            title: "Style",
+            sections: [        
+                {   // Style
+                    name: "style",
+                    title: "Text and style",
+                    properties: [ id$, title$, fontSize$, backColor$, textColor$, opacity$ ]
+                },         
+                {   // Position & size
+                    name: "pos_size",
+                    title: "Position and size",
+                    properties: [x$, y$, w$, h$, rotate$, radius$] 
+                }, 
+                
+            ] // sections
+        },
+        otherTab
+    ]
+}
+
+
+
+export const connectionModel: ObjectInspectorModel = {
+    tabs: [ 
+        {
+            title: "Style",
+            sections: [        
+                {   // Style
+                    name: "style",
+                    title: "Style",
+                    properties: [ 
+                        { name: "type",  label: "Type",  type: PropertyType.SELECT, editorFullsize: true, editorOptions: { items: [ ConnectionType.LINE, ConnectionType.CURVE] }},
+                        { name: "style", label: "Style", type: PropertyType.SELECT, editorFullsize: true, editorOptions: { items: [ ConnectionStyle.SOLID, ConnectionStyle.DASHED, ConnectionStyle.DOTTED] }},
+                        { name: "thick", label: "Thick", type: PropertyType.RANGE,  editorFullsize: true, editorOptions: { min: 1, max: 10, step: 1 }},
+                        
+                        { name: "backgroundColor",  label: "Color", type: PropertyType.COLOR }
+                    ]
+                },         
+            ] // sections
+        } // tab
+    ] // tabs
+}
+
